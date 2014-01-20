fbd293f339f4889b6260a6689eccea2c3c19c325
Mathieuu Gosbee <mail@matbee.com>
2014-01-20 00:30:08 -0500

    base64 hash stuff

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ var wormhole = function (socket) {
 	this.setupListeners();
 	this.setSocket(socket);
 
-	this._rpc = {};
+	this._availablerpc = {};
+	this.rpc = {};
+
+	this.__hash__ = "";
 };
 wormhole.prototype.__proto__ = events.EventEmitter.prototype;
 wormhole.prototype.setSocket = function(socket) {
@@ -27,21 +30,41 @@ wormhole.prototype.isWormhole = function(cb) {
 	}
 };
 wormhole.prototype.setupListeners = function() {
-	// this.on("removeListener", this._unsubscribe);
 	var self = this;
 	this.on("newListener", function (event, func) {
 		if (event != "removeListener" && event != "newListener") {
 			// Oh, this is an RPC, Add the fucker!
-			this._rpc[event] = func;
+			self.addFunction(event, func);
 		}
 	});
 };
 wormhole.prototype.setupSocketListeners = function() {
+	var self = this;
+	//Original func
 	this.socket.on("isWormhole", function (cb) {
 		cb(null, true);
+		self.syncFunctions();
+	});
+
+	this.socket.on("rpc", function (data) {
+		if (data.__hash__ != self.__hash__) {
+			self.emit("outofsync", self);
+		}
 	});
 };
 wormhole.prototype.addFunction = function(func, cb) {
-	this._rpc[func] = cb;
+	this._availablerpc[func] = cb;
+	var str = Object.keys(this._availablerpc).join("");
+	this.__hash__ = new Buffer(str).toString('base64');
+};
+wormhole.prototype.syncFunctions = function() {
+	this.socket.emit("functions", Object.keys(this._availablerpc));
+};
+wormhole.prototype.addClientFunction = function(func) {
+	var self = this;
+	this.rpc[func] = function () {
+		var args = ["rpc", func].concat([].slice.call(arguments));
+		self.socket.emit.apply(self, args);
+	};
 };
 module.exports = wormhole;
